08425d416e8de6f03e27a24e60683d68ebda3eb1
Donald Lee <dlqs@gmx.com>
2021-07-28 21:24:52 +0800

    untar lua before configure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Extra Lua
+tar -xf lua-5.3.6.tar.gz
+
 # This file exists to document the proper way to initialize autotools,
 # and so that those used to the presence of bootstrap.sh or autogen.sh
 # will have an eaiser time.
